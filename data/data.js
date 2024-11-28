68ee5729b99fee5c1227cd602e7e63b875cce089
sebastianrjz <tomas24sebastian@gmail.com>
2024-11-28 20:15:49 +0100

    proyecto

--- data/data.js
+++ data/data.js
@@ -28,13 +28,13 @@ const data = {
     }
   ],
   skills: [
-    '/public/src/174854 1.png',
-    '/public/src/CSS3_icon-icons 1.png',
-    '/public/src/javascript-logo-javascript-icon-transparent-free-png 1.png',
-    '/public/src/node-js-icon-1817x2048-g8tzf91e 1.png',
-    '/public/src/React-icon 1.png',
-    '/public/src/file_type_vscode_icon_130084 1 (1).png',
-    '/public/src/25231 1.png'
+    'https://icones.pro/wp-content/uploads/2021/05/icone-html-jaune.png',
+    'https://cdn-icons-png.flaticon.com/512/5968/5968242.png',
+    'https://logos-world.net/wp-content/uploads/2023/02/JavaScript-Logo.png',
+    'https://img.icons8.com/fluent/200/node-js.png',
+    'https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png',
+    'https://img.icons8.com/?size=512&id=9OGIyU8hrxW5&format=png',
+    'https://cdn-icons-png.flaticon.com/512/25/25231.png'
   ],
 
   projects: [
